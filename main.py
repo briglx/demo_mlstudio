1cd861d4f2c5faf80653496dff401c0db113f910
Brig Lamoreaux <briglx@users.noreply.github.com>
2023-12-20 18:59:57 +0000

    add hello world

---
+++ main.py
@@ -0,0 +1,12 @@
+"""Example of a main file to run the program."""
+#!/usr/bin/env python3
+
+import sys
+
+
+def main():
+    """Main function."""
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
